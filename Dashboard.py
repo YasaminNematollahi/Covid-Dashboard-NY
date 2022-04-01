dba2a77c7558f323c7d340c32f4e04dd18981a31
Yasamin Nematollahi <yasamin.nematollahi@cri-paris.org>
2022-04-01 15:25:35 +0200

    Changed the dataset

--- Dashboard.py
+++ Dashboard.py
@@ -5,9 +5,6 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+import matplotlib.pyplot as plt
+df = pd.read_csv(r"C:\Users\yasam\Covid-Dashboard-NY\time_series_covid19_confirmed_global.txt")
+df
