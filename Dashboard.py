3468683fcc296e765562a7336756c88462259417
Yasamin Nematollahi <yasamin.nematollahi@cri-paris.org>
2022-04-01 15:58:49 +0200

    added all

--- Dashboard.py
+++ Dashboard.py
@@ -5,6 +5,8 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 df = pd.read_csv(r"time_series_covid19_confirmed_global.txt")
 df
+df.columns
