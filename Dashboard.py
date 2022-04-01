e3981ca8edc9d4a4f4e90295a5a656824b7fbc28
Yasamin Nematollahi <yasamin.nematollahi@cri-paris.org>
2022-04-01 15:29:04 +0200

    Changed the dataset

--- Dashboard.py
+++ Dashboard.py
@@ -6,5 +6,5 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv(r"Covid-Dashboard-NY\time_series_covid19_confirmed_global.txt")
+df = pd.read_csv(r"time_series_covid19_confirmed_global.txt")
 df
