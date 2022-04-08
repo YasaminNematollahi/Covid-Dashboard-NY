6d4014193635bf466eeaff17c2e25c168cd5a400
Natalia Savelyeva <nssaveleva.i@gmail.com>
2022-04-08 10:07:45 +0200

    dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -6,5 +6,7 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+st.write("# COVID Dashboard")
+st.write("#### Yasamin & Natalia")
 df = pd.read_csv(r"time_series_covid19_confirmed_global.txt")
 df
