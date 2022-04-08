2ad618762459bdc0681ba4c2645e3be613d8eb99
Yasamin Nematollahi <yasamin.nematollahi@cri-paris.org>
2022-04-08 09:59:56 +0200

    New one

--- Dashboard.py
+++ Dashboard.py
@@ -6,6 +6,14 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv(r"time_series_covid19_confirmed_global.txt")
+st.write("# COVID Dashboard")
+st.write("#### Yasamin & Natalia")
+df = pd.read_csv(r"time_series_covid19_deaths_global.txt")
 df
 df.columns
+fig = plt.figure()
+plt.plot(df[df["Country/Region"]=="France"].iloc[[0]].iloc[:,4:].values[0])
+
+
+# Plots the chart
+st.pyplot(fig)
