becc9671fb484d5a131e29566eb653b912d1e051
Natalia Savelyeva <nssaveleva.i@gmail.com>
2022-04-22 11:54:19 +0200

    Fixing date

--- Dashboard.py
+++ Dashboard.py
@@ -9,12 +9,11 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import datetime as dt
 
-#cashing data
-@st.cache(allow_output_mutation = True)
 
-# function for reading data from url
+@st.cache(allow_output_mutation = True)
 def readdata(url): 
     data = pd.read_csv(url)
+    data["date"] = pd.to_datetime(data["date"]).dt.date
     return data
 
 st.write("# COVID Dashboard")
@@ -34,14 +33,19 @@ start_date = min(covid['date'])
 end_date = max(covid['date'])
 
 start_date2, end_date2 = st.date_input("Pick a date range", [start_date, end_date])
-covid1 = covid.loc[(covid.date >= pd.to_datetime(start_date2)) & (covid.date <= pd.to_datetime(end_date2))]
+covid1 = covid.loc[(covid.date >= start_date2) & (covid.date <= end_date2)]
 
 
 # select the mode for data
-mode = st.sidebar.radio("Select the mode for displaying", ("Covid-19 cases", "Covid-19 deaths"))
+mode = st.sidebar.radio(" Select the mode for displaying", ("Covid-19 cases","Covid-19 rolling average", "Covid-19 cumulated","Covid-19 deaths"))
+#submode = st.si
 
 if mode == "Covid-19 cases":
   fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "new_cases_per_million", title = " and ".join(country), color = "location")
+elif mode == "Covid-19 cumulated":
+  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "total_cases_per_million", title = " and ".join(country), color = "location")
+elif mode == "Covid-19 rolling average":
+  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "new_cases_smoothed_per_million", title = " and ".join(country), color = "location")
 elif mode == "Covid-19 deaths":
-  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "new_deaths_per_million", title = " and ".join(country), color = "location")
+  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "total_deaths_per_million", title = " and ".join(country), color = "location")
 st.plotly_chart(fig)
