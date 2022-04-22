d02553d4a2c4493701c292a0be19de1095b7d9ff
Natalia Savelyeva <nssaveleva.i@gmail.com>
2022-04-22 09:59:23 +0200

    Choosing date

--- Dashboard.py
+++ Dashboard.py
@@ -32,22 +32,30 @@ covid['month'] = covid['date'].dt.month
 clist = covid['location'].unique()
 country = st.sidebar.multiselect("Select a country:",clist, default = ["Iran"])
 
-# select the mode for date
-mode = st.sidebar.radio("Select the mode for displaying", ("Covid-19 cases", "Covid-19 deaths"))
-
-if mode == "Covid-19 cases":
-  fig = px.line(covid[ covid['location'].isin(country) ], x = "date", y = "new_cases_per_million", title = " and ".join(country), color = "location")
-elif mode == "Covid-19 deaths":
-  fig = px.line(covid[ covid['location'].isin(country) ], x = "date", y = "new_deaths_per_million", title = " and ".join(country), color = "location")
-st.plotly_chart(fig)
-
-
 format="MM/DD/YY"
 start_date = min(covid['date'])
 end_date = max(covid['date'])
+#start_dt = st.sidebar.date_input('Start date', value=covid['date'].min())
+#end_dt = st.sidebar.date_input('End date', value=covid['date'].max())
 st.write(start_date)
 st.write(end_date)
-slider = st.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date)
+#slider = st.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date)
+start_date2, end_date2 = st.date_input("Pick a date range", [start_date, end_date])
+covid1 = covid.loc[(covid.date >= pd.to_datetime(start_date2)) & (covid.date <= pd.to_datetime(end_date2))]
+#covid1
+
+# select the mode for data
+mode = st.sidebar.radio("Select the mode for displaying", ("Covid-19 cases", "Covid-19 deaths"))
+
+if mode == "Covid-19 cases":
+  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "new_cases_per_million", title = " and ".join(country), color = "location")
+elif mode == "Covid-19 deaths":
+  fig = px.line(covid1[ covid1['location'].isin(country) ], x = "date", y = "new_deaths_per_million", title = " and ".join(country), color = "location")
+st.plotly_chart(fig)
+
+
+
+
 
 #st.slider("Time slider", min_value=min(covid['date']), max_value=max(covid["date"]), format="MM/DD/YY - hh:mm")
 
